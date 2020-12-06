09c6f93474a88572f58b987ecea753ce850699ee
valfeo14 <ya.valfeo@yandex.ru>
2020-12-06 10:30:57 +0300

    Create s1.py

---
+++ s1.py
@@ -0,0 +1,17 @@
+# WS server example
+
+import asyncio
+import websockets
+import numpy as np
+
+arr = np.random.rand(5)
+
+async def hello(websocket, path):
+    barr = await websocket.recv()
+    arr = np.frombuffer(barr)
+    print("recieve  ", arr)
+
+start_server = websockets.serve(hello, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
