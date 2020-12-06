0fe638b3c49a6bb500058e7173e9b02cacfd8dfd
valfeo14 <ya.valfeo@yandex.ru>
2020-12-06 14:39:43 +0300

    com1

--- s1.py
+++ s1.py
@@ -9,9 +9,10 @@ arr = np.random.rand(5)
 async def hello(websocket, path):
     barr = await websocket.recv()
     arr = np.frombuffer(barr)
-    print("recieve  ", arr)
+    print("recieve = ", arr)
 
 start_server = websockets.serve(hello, "localhost", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+#first comment
