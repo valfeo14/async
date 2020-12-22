f96bc5a5b9f9c12eba43fcf23055c17940b741e8
valfeo14 <ya.valfeo@yandex.ru>
2020-12-22 12:57:21 +0300

    later

--- 1.py
+++ 1.py
@@ -1,29 +1,19 @@
 import asyncio
-import time
 
-async def say_after(delay, what):
-	while True :
-	    await asyncio.sleep(delay)
-	    print(what)
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number + 1):
+        print(f"Task {name}: Compute factorial({i})...")
+        await asyncio.sleep(1)
+        f *= i
+    print(f"Task {name}: factorial({number}) = {f}")
 
 async def main():
-    task1 = asyncio.create_task(
-        say_after(4, 'hello'))
-
-    task2 = asyncio.create_task(
-        say_after(1, 'world'))
-
-    task3 = asyncio.create_task(
-        say_after(2, 'hoppa'))
-
-    print(f"started at {time.strftime('%X')}")
-
-    # Wait until both tasks are completed (should take
-    # around 2 seconds.)
-    await task1
-    await task2
-    await task3
-
-    print(f"finished at {time.strftime('%X')}")
+    # Schedule three calls *concurrently*:
+    await asyncio.gather(
+        factorial("A", 2),
+        factorial("B", 3),
+        factorial("C", 4),
+    )
 
 asyncio.run(main())
